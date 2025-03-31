fd2dbd7ef13c30164986c892eb22e34b7f593c3a
ronakmaheshwari077 <ronakmaheshwari077@google.com>
2025-03-31 20:23:11 +0530

    Added Backend

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express"
+import morgan from "morgan"
+import zod from "zod"
+import cors from "cors"
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+import router from "./routes/index.js"
+
+const app = express()
+const port = process.env.PORT || 3000
+export const JWT_SECRET = "123456"
+export const saltrounds = 10;
+
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(cors())
+app.use("/api/v1/",router);
+
+app.listen(port,()=>{
+    console.log(`Server Running On http://localhost:${port}`)
+})
